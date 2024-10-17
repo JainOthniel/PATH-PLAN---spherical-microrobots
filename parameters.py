8cf74c1b4701ff5af1e9b4dfc840e2352dcaaa68
JAIN OTHNIEL RAMESH <jainramesh136@gmail.com>
2024-10-17 14:08:28 +0530

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -13,7 +13,7 @@ class Parameters:
     viscosity: float
     radius_robot: float
     radius_obstacle: float
-    force: np.ndarray
+    Force: np.ndarray
     position_robot: np.ndarray
     position_obstacle: np.ndarray
 
